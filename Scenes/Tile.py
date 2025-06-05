59b1bb80ef1b21b623dcb4d97c325ed96969da14
YellowFang76 <dfr5720@gmail.com>
2025-06-06 02:30:38 +0300

    Чистка репозитория от ненужных файлов

--- Scenes/Tile.py
+++
@@ -1,9 +0,0 @@
-from godot import exposed, export, Node2D
-from godot import *
-from godot import exposed, Node2D
-
-@exposed
-class Tile(Node2D):
-	def dig(self):
-		print(f"Tile at {self.position} is being dug!")
-		self.queue_free()
